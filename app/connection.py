0136cc151887d20388e73227e4cc4985e138e289
elazar meijers <meijers212@gmail.com>
2026-02-05 11:23:28 +0200

    add dockerfilr and sum funqtunality

--- app/connection.py
+++ app/connection.py
@@ -1,28 +1,33 @@
+import json
 from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
+from os import getenv
 
-load_dotenv()
 
-db_host = os.getenv("DB_HOST", "localhost")
-db_port = os.getenv("DB_PORT", 27017)
-db_name = os.getenv("DB_NAME", "my_db")
-col_name = os.getenv("COL_NAME","week_16")
+mongo_uri = getenv("MONGO_URI", "mongodb://mongodb-gerstnir-dev.apps.rm2.thpm.p1.openshiftapps.com:27017/")
+mongo_db = getenv("MONGO_DB", "my_db")
+mongo_collection = getenv("MONGO_COLLECTION", "testcollection")
+file_path = './employee_data_advanced.json'
 
-uri = f'mongodb://{db_host}:{db_port}'
+# Making Connection
+myclient = MongoClient(mongo_uri)
 
-client = MongoClient(uri)
+# database
+db = myclient[mongo_db]
 
-db = client[db_name]
+# Created or Switched to collection
+# names: GeeksForGeeks
+Collection = db[mongo_collection]
 
-col = db[col_name]
+# Loading or Opening the json file
+with open(file_path) as file:
+    file_data = json.load(file)
 
+# Inserting the loaded data in the Collection
+ins_result = Collection.insert_many(file_data)
+print(f"Data inserted to MongoDB. Documents inserted: {len(ins_result.inserted_ids)}")
 
 
 def get_collection():
-    return client[db_name][col_name]
-
-
-
+    return Collection
 
 
